06066f0a626d70f4a4873b1706832de4e2168ce9
afterlunch <95327249+afterlunch@users.noreply.github.com>
2022-09-28 12:47:52 +0800

    Update 自动裁切.py

--- 自动裁切.py
+++ 自动裁切.py
@@ -9,7 +9,7 @@ a = input('本程序工作目录为C:png\n请输入需要截图的左坐标a')
 b = input('请输入需要截图的上坐标b')
 c = input('请输入需要截图的右坐标c')
 d = input('请输入需要截图的下坐标d')
-rootdir = r'C:\Users\zWX1130317\Desktop\56565656\png'
+rootdir = r'C:\Users\17\Desktop\56565656\png'
 for parent, dirnames, filenames in os.walk(rootdir):#遍历每一张图片
     for filename in filenames:
         print('parent is :' + parent)
@@ -22,4 +22,4 @@ for parent, dirnames, filenames in os.walk(rootdir):#遍历每一张图片
         #img.show()
         box1 = (500, 600,800, 900)#设置左、上、右、下的像素
         image1 = img.crop(box1) # 图像裁剪
-        image1.save(r'C:\Users\zWX1130317\Desktop\56565656\png'+'\\'+filename) #存储裁剪得到的图像5
+        image1.save(r'C:\Users\317\Desktop\56565656\png'+'\\'+filename) #存储裁剪得到的图像5
